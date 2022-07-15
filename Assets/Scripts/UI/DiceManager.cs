c4a7e6737697154c68815f765d6cd2e7f86e5b3d
Lee Martin <lee.martin@pliminus.co.uk>
2022-07-15 21:56:07 +0100

    Added: Dice Buttons

--- Assets/Scripts/UI/DiceManager.cs
+++ Assets/Scripts/UI/DiceManager.cs
@@ -1,8 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 using System;
+using TMPro;
+
+#nullable enable
+#nullable disable warnings
 
 namespace Dyelaga {
 
@@ -12,7 +17,32 @@ namespace Dyelaga {
 
         public Button rerollButton;
 
-        private (int, int, int) _RGBDiceNumbers;
+        public GameObject dicebuttonPrefab;
+
+        public GameObject dicePoolSlot;
+
+        private (int, int, int) _RGBSlotNumbers = (0, 0, 0);
+
+        private List<GameObject> _diceButtons = new List<GameObject>();
+
+        private enum DiePosition {
+            Red,
+            Green,
+            Blue,
+            Pool
+        }
+
+        private class Die {
+            public int Value;
+            public DiePosition Position;
+            public GameObject? GameObject;
+        }
+
+        private List<Die> _Dice = new List<Die>() {
+            new Die { Value = 1, Position = DiePosition.Pool },
+            new Die { Value = 1, Position = DiePosition.Pool },
+            new Die { Value = 1, Position = DiePosition.Pool }
+        };
 
         // Start is called before the first frame update
         void Start()
@@ -24,25 +54,41 @@ namespace Dyelaga {
                 Console.Error.WriteLine("DiceManager needs a rerollButton to interact");
 
             Button btn = rerollButton.GetComponent<Button>();
-            btn.onClick.AddListener(TaskGenerateRandomNumbers);
-            TaskGenerateRandomNumbers();
+            btn.onClick.AddListener(TaskReRollDice);
+            TaskReRollDice();
         }
 
         // Update is called once per frame
         void Update()
         {
-            dicePool.Red.NumberOfBullets = _RGBDiceNumbers.Item1;
-            dicePool.Green.NumberOfBullets = _RGBDiceNumbers.Item2;
-            dicePool.Blue.NumberOfBullets = _RGBDiceNumbers.Item3;
+            dicePool.Red.NumberOfBullets = _RGBSlotNumbers.Item1;
+            dicePool.Green.NumberOfBullets = _RGBSlotNumbers.Item2;
+            dicePool.Blue.NumberOfBullets = _RGBSlotNumbers.Item3;
         }
 
-        void TaskGenerateRandomNumbers() {
-            _RGBDiceNumbers = (
-                UnityEngine.Random.Range(1, 6),
-                UnityEngine.Random.Range(1, 6),
-                UnityEngine.Random.Range(1, 6)
-            );
+        void TaskReRollDice() {
+            // _RGBSlotNumbers = (
+            //     UnityEngine.Random.Range(1, 6),
+            //     UnityEngine.Random.Range(1, 6),
+            //     UnityEngine.Random.Range(1, 6)
+            // );
+
+            _Dice.ForEach(d => Destroy(d.GameObject));
+
+            int mod = -50;
+            _Dice = _Dice.Select(x => { 
+                x.Value = UnityEngine.Random.Range(1, 6); 
+                x.Position = DiePosition.Pool;
+                x.GameObject = Instantiate (dicebuttonPrefab, new Vector3(dicePoolSlot.transform.position.x + mod ,dicePoolSlot.transform.position.y, dicePoolSlot.transform.position.z) , Quaternion.identity);
+                x.GameObject.transform.SetParent(dicePoolSlot.transform);
+                Button btn = x.GameObject.GetComponent<Button>();
+                btn.GetComponentInChildren<TMP_Text>().text = x.Value.ToString();
+                mod = mod + 50;
+                return x; 
+            }).ToList();
         }
+
+        
     }
 
 }
