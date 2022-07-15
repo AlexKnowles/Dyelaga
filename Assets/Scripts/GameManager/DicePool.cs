6ea353766c6be710938e1cb41f450e38591b6689
Alex Knowles <git@alexknowles.info>
2022-07-15 20:14:15 +0100

    Game manager base

---
+++ Assets/Scripts/GameManager/DicePool.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using System.Runtime.Serialization;
+
+
+namespace Dyelaga
+{
+    public class DicePool : MonoBehaviour
+    {
+        public ColourDice Red;
+        public ColourDice Blue;
+        public ColourDice Green;
+    }
+
+
+    [Serializable]
+    public class ColourDice {
+        [Range(0, 10)]
+        public int NumberOfBullets;
+    }
+
+}
