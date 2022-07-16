6168eef6e66e6f5345c792236ed82754ca6548d7
Alex Knowles <git@alexknowles.info>
2022-07-16 12:40:05 +0100

    basic enemy spawn

---
+++ Assets/Scripts/Editor/WaveEditor.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+namespace Dyelaga.Levels
+{
+    [CustomEditor(typeof(Wave))]
+    public class WaveEditor : Editor 
+    {
+
+        public override void OnInspectorGUI()
+        {
+            DrawDefaultInspector();
+
+            Wave wave = (Wave)target;
+            if(GUILayout.Button("Spawn Enemy"))
+            {
+                wave.SpawnEnemy();
+            }
+        }
+
+    }
+}
--- Assets/Scripts/Levels/Wave.cs
+++ Assets/Scripts/Levels/Wave.cs
@@ -12,5 +12,14 @@ namespace Dyelaga.Levels
         public GameObject Enemy;
         public int NumberOfEnemies;
         public float ReleaseIntervalSeconds = 0.5f;   
+
+        public void SpawnEnemy()
+        {
+            Vector3 firstPosition = Path.GetComponent<LineRenderer>().GetPosition(0);
+
+            firstPosition = new Vector3(firstPosition.x, firstPosition.y, 0);
+
+            Instantiate(Enemy, firstPosition, new Quaternion(), transform);
+        }
     }
 }
